97b7536f26b4690dcd890a29a131d59415bd26e1
Mateus Pereira <mateuspereira@MacBook-Pro-de-Mateus.local>
2013-11-16 21:22:50 -0200

    Initial Commit

---
+++ TrabalhoDijkstra/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TrabalhoDijkstra
+//
+//  Created by Mateus Pereira on 16/11/13.
+//  Copyright (c) 2013 Mateus Pereira. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
